a78f247459dce61a9e9a1919872c62f59c9a3be9
zhuy-nguyenz <graio911@gmail.com>
2021-06-08 23:05:53 +0700

    100k array

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -1,14 +1,36 @@
 package com.codegym;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         StopWatch watch = new StopWatch();
+        Random random = new Random();
         watch.start();
-        for (int i = 0; i < 100000; i++) {
-            System.out.println(i);
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
         }
+        sort(array);
         watch.stop();
-        System.out.println(watch.getElapsedTime());
+        System.out.println(Arrays.toString(array));
+        System.out.println(watch.getElapsedTime() + " milisec");
+    }
+
+    public static void sort(int[] arr) {
+        int size = arr.length;
+        for (int step = 0; step < size - 1; step++) {
+            int min = step;
+            for (int i = step + 1; i < size; i++) {
+                if (arr[i] < arr[min]) {
+                    min = i;
+                }
+            }
+            int temp = arr[step];
+            arr[step] = arr[min];
+            arr[min] = temp;
+        }
     }
 }
